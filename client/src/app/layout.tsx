d807fe548e7c2ee427200728ddcd073886f9f5f9
Ambreensheikh <160266554+Ambreensheikh@users.noreply.github.com>
2026-04-28 13:47:08 +0500

    Updated SEO metadata

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -4,19 +4,22 @@ import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-// 1. Metadata Object
 export const metadata: Metadata = {
+  // Yahan apna Vercel link dalen taake absolute paths sahi kaam karein
+  metadataBase: new URL('https://my-portfolio-orpin-one-2z7wrdiu02.vercel.app/'), 
+  
   title: 'Ambreen Sheikh | AI Automation & Full-Stack Engineer',
-  description: 'Specialist in AI-Automation, Event-Driven Architecture, Next.js, Node.js, and Python. Expert in OpenAI Agent SDK, n8n automation, and DevOps,AWS,PostgreSQL, MongoDB. Building autonomous agents and event-driven systems.',
+  description: 'Specialist in AI-Automation, Event-Driven Architecture, Next.js, Node.js, and Python. Expert in OpenAI Agent SDK, n8n automation, and DevOps, AWS, PostgreSQL, MongoDB.',
+  
   openGraph: {
     title: 'Ambreen Sheikh | AI Automation & Full-Stack Engineer',
     description: 'Building autonomous agents and event-driven systems.',
+    url: 'https://my-portfolio-orpin-one-2z7wrdiu02.vercel.app/', // Yahan bhi link add kar dein
     siteName: 'Ambreen Portfolio',
     type: 'website',
   },
 };
 
-// 2. The Default Export (Ye hona zaroori hai)
 export default function RootLayout({
   children,
 }: {
